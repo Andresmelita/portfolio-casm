7230b7e974db72cf2b7c585a121c4844d58b17e4
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2022-12-03 19:10:14 -0300

    about changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,23 +18,23 @@ const About = () => {
                     <div className='paragraph'>Soy una persona organizada, dedicada y creativa, me encuentro constantemente aprendiendo, estaré siempre dispuesto a afrontar nuevos retos y desarrollar nuevas habilidades.</div>
                     <div className='about-information'>
                         <div className='information-education'>
-                            <h3 className='education-title'><FaUserGraduate/> Education</h3>
+                            <h3 className='education-title'><FaUserGraduate/> Educación</h3>
                             <div className='education'>
-                                <ol className='career'>Full-Stack Development and Computer Science</ol>
+                                <ol className='career'>Desarrollo Full-Stack y Ciencias de la Computación</ol>
                                 <ol className='year'>2022 - currently</ol>
                                 <ol className='institution'>Academlo</ol>
                             </div>
                             <div className='education'>
-                                <ol className='career'>Mining Engineering (Bachelor of Engineering) </ol>
+                                <ol className='career'>Ingeniero de Ejecución en minas (Licenciado en Ingeniería) </ol>
                                 <ol className='year'>2016 - 2021</ol>
                                 <ol className='institution'>Universidad de Santiago de Chile</ol>
                             </div>
                         </div>
                         <div className='information'>
-                            <h3 className='myInformation-title'><FaAddressBook/> Information</h3>
+                            <h3 className='myInformation-title'><FaAddressBook/> Información</h3>
                             <div className='myInformation'>
                                 <p className='datum'><span>Age: </span>27</p>
-                                <p className='datum'><span>Degree: </span>Bachelor</p>
+                                <p className='datum'><span>Degree: </span>Licenciado</p>
                                 <p className='datum'><span>Phone: </span>+569 7938 4744</p>
                                 <p className='datum'><span>Location: </span>Santiago, Chile</p>
                                 <p className='datum'><span>Mail: </span>carlos.salazar.me@usach.cl</p>
