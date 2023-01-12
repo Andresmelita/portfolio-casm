baa73eb800178657f74438bb464b334466839fa1
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-01-12 11:34:49 -0300

    added a new loader

---
+++ src/components/Loading/LoadingTwo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './loadingTwo.scss'
+
+const Loading = () => {
+    return (
+        <div className='container-loader'>
+            <div className="loader">
+                <span>&#123;</span><span>&#125;</span>
+            </div>
+        </div>
+
+    )
+}
+
+export default Loading
